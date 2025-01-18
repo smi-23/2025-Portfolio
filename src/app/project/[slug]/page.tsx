ce461251da41fae69f78f7a4411d0a5ca46c1560
smi-23 <latack789@naver.com>
2025-01-18 22:22:03 +0900

    Fix: projectDtailPage의param에Promise 처리 추가

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,15 +1,15 @@
+import { fetchNotionDb } from "@/lib/notion";
 import { FetchNotionPage } from "@/lib/notionPage";
 import Renderer from "@/components/notion/Renderer";
-import { fetchNotionDb } from "@/lib/notion";
-import { notFound } from "next/navigation";
 import { PROJECT } from "@/type/project";
+import { notFound } from "next/navigation";
 
-interface ProjectDetailPageProps {
-  params: { slug: string };
-}
+// interface ProjectDetailPageProps {
+//   params: { slug: string };
+// }
 
-export default async function ProjectDetailPage({ params }: ProjectDetailPageProps) {
-  const { slug } = params;
+export default async function ProjectDetailPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
   const projects: PROJECT[] = await fetchNotionDb();
   const project = projects.find((project) => project.slug === slug);
 
