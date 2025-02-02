866f36332b981d667dc8fd04ae60a6b975d4c598
smi-23 <latack789@naver.com>
2025-02-03 05:17:39 +0900

    Fix: 환경 변수 이름 맞게 설정

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -9,7 +9,9 @@ interface ProjectDetailPageProps {
 }
 
 async function getRecodeMap(pageId: string) {
-  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+    ? `https://${process.env.NEXT_PUBLIC_BASE_URL}`
+    : "http://localhost:3000";
   const res = await fetch(`${baseUrl}/api/notion?pageId=${pageId}`, {
     next: { revalidate: 3600 },
   });
